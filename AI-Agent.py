77960df78af2c8a8aaa92bda5656a18d5635f300
Yashshah0502 <113469955+Yashshah0502@users.noreply.github.com>
2025-07-14 21:16:26 -0400

    Initialize ReAct agent with Groq LLM and Tavily search tool for AI news queries
    - Set up a ReAct-style agent using `create_react_agent` from LangGraph
    - Integrated Groq’s LLaMA 3.3 70B model (`groq_llm`) for reasoning
    - Added TavilySearch tool to enable real-time news retrieval
    - Invoked the agent with a sample query on AI news and filtered final AI response
    - Removed unsupported system prompt injection; to be added via model config if needed

--- AI-Agent.py
+++ AI-Agent.py
@@ -9,10 +9,27 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 
 from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
-from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_tavily import TavilySearch
 
-openai_llm = ChatOpenAI(model = "gpt-4o-min")
-groq_llm = ChatGroq(model = "llama-3.3-70b-versatile")
+openai_llm = ChatOpenAI(model="gpt-4o-mini")
+groq_llm = ChatGroq(model="llama-3.3-70b-versatile", api_key=GROO_API_KEY)
 
-search_tool = TavilySearchResults(max_results = 2)
+search_tool = TavilySearch(max_results=2, api_key=TAVILY_API_KEY)
 
+system_prompt = "Act as an AI chatbot who is smart and friendly"
+
+from langgraph.prebuilt import create_react_agent
+from langchain_core.messages.ai import AIMessage
+
+
+# Try without state_modifier first to see if that's the issue
+agent = create_react_agent(
+    model=groq_llm,
+    tools=[search_tool])
+
+query = "tell me about the latest news in AI"
+response = agent.invoke({"messages": [("user", query)]})
+messages = response.get("messages")
+ai_messages=[message.content for message in messages if isinstance(message, AIMessage)]
+print(ai_messages[-1])
+# print(response)
